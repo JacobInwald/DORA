55cb9e49a6238aae44265c85fe350649e1d6e759
JacobInwald <Jacob.Inwald@Hotmail.com>
2024-01-29 14:35:31 +0000

    mapping structure added

--- src/libraries/mapping.py
+++ src/libraries/mapping.py
@@ -1,8 +1,35 @@
 import numpy as np
+from matplotlib import pyplot as plt
 
 
-with open('lidar01.csv') as f:
-    data = f.readlines()
-    data = [line.split(',') for line in data]
-    data = np.array(data)
-    
+def bresenham(start, end):
+    pass
+
+
+def floodfill(start):
+    pass
+
+
+def occupancy_grid(lidar_data):
+    pass
+
+
+def load_data():
+    with open('data/lidar01.csv') as f:
+        data = f.readlines()
+        data = [line.strip() for line in data]
+        data = [line.split(',') for line in data]
+        data = np.array(data).astype(np.double)
+        angles = data[:, 0]
+        distances = data[:, 1]
+    return angles, distances
+
+angles, distances = load_data()
+print(angles)
+print(distances)
+
+xs = distances * np.cos(angles)
+ys = distances * np.sin(angles)
+
+plt.plot(xs, ys, 'ro')  
+plt.show()
