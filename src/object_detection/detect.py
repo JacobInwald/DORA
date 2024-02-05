e6ef98cd3ea32ff0e07efaf4d71b0e986400e300
teethoe <ardithhowaiman@gmail.com>
2024-02-05 18:16:59 +0000

    add weights for object detection

--- src/object_detection/detect.py
+++ src/object_detection/detect.py
@@ -6,5 +6,12 @@ class Detect:
     def __init__(self, model_file):
         self.model = YOLO(model_file)
 
-    def predict(self, file):
-        return self.model(file)
+    def predict(self, file, conf=0.5):
+        self.model(file, save=True, conf=conf)
+
+
+if __name__ == '__main__':
+    path = 'weights/best_1.pt'
+    model = Detect(path)
+    vid_path = '../../data/vid/20240201_dav.mp4'
+    model.predict(vid_path)
