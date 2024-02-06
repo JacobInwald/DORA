50c5e5360ac29b9a48c32a0c4c02063409e5dbaf
teethoe <ardithhowaiman@gmail.com>
2024-02-06 15:01:53 +0000

    fix object detection demo

--- src/object_detection/detect.py
+++ src/object_detection/detect.py
@@ -6,12 +6,15 @@ class Detect:
     def __init__(self, model_file):
         self.model = YOLO(model_file)
 
-    def predict(self, file, conf=0.5, save=False):
-        self.model(file, conf=conf, save=save)
+    def predict(self, file, conf=0.5, save=True):
+        self.model.predict(file, conf=conf, save=save)
+
+    def predictions(self, file, conf=0.5, save=False):
+        return self.model.predict(file, conf=conf, save=save)
 
 
 if __name__ == '__main__':
     path = 'weights/best_32.pt'
     model = Detect(path)
     vid_path = '../../data/vid/20240201_dav.mp4'
-    model.predict(vid_path, save=True)
+    model.predict(vid_path)
