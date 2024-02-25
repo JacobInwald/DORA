d2974c158314f68d09e0c687f2f7b88db8525cb0
teethoe <ardithhowaiman@gmail.com>
2024-02-25 15:43:41 +0000

    add enum for sweeper moves

--- src/actuators/actuators/wheels.py
+++ src/actuators/actuators/wheels.py
@@ -1,3 +1,18 @@
 import rclpy
 from rclpy.node import Node
-from doramsgs
+from dorasrvs.srv import WheelsCommand
+
+
+class Wheels(Node):
+    """
+    Represents the wheels.
+
+    Wait for controller wheel movement service call.
+    Control wheels to move robot according to service message.
+    """
+    def __init__(self):
+        super.__init__('wheels')
+        self.service_ = self.create_service(WheelsCommand, 'wheels', self.callback)
+
+    def callback(self, msg):
+        if msg.type ==
