deed0912caa06d31390e62aed64e4c2d7a9e0e21
TinaZw <s2047893@ed.ac.uk>
2024-02-26 17:48:19 +0000

    Fix simple error, change the some calling of the method to stay the same page with router. Add some TODO stuff

--- src/control/control/controller.py
+++ src/control/control/controller.py
@@ -1,10 +1,12 @@
+import math
+import numpy as np
 import rclpy
 from rclpy.node import Node
-from dora_msgs.msg import Toy, Pose, Toys
+from dora_msgs.msg import Toy, Pose
 from dora_srvs.srv import JobCmd, LdsCmd, SweeperCmd, WheelsCmd
-from .router import Router
-from .job import DoraJob
-import numpy as np
+from router import Router
+from occupancy_map import OccupancyMap
+from job import DoraJob
 
 
 class Controller(Node):
@@ -16,16 +18,16 @@ class Controller(Node):
     """
 
     def __init__(self):
-        rclpy.init()
         super().__init__('controller')
         self.service_ = self.create_service(JobCmd, 'job', self.switch)
-        self.toy_sub_ = self.create_subscription(Toys, 'toys', self.toy_callback, 10)
+        self.toy_sub_ = self.create_subscription([Toy], 'toys', self.toy_callback, 10)
         self.gps_sub_ = self.create_subscription(Pose, 'gps', self.gps_callback, 10)
-        self.cli_node_ = Node('control_client')
+        self.cli_node_ = Node()
         self.lds_cli_ = self.cli_node_.create_client(LdsCmd, 'lds_service')
         self.wheels_cli_ = self.cli_node_.create_client(WheelsCmd, 'wheels')
         self.sweeper_cli_ = self.cli_node_.create_client(SweeperCmd, 'sweeper')
         self.router = Router()
+        self.map = OccupancyMap()
         self.toy = None
 
     def switch(self, msg):
@@ -41,7 +43,7 @@ class Controller(Node):
             return self.unload_request()
         return False
 
-    def toy_callback(self, msg: Toys):
+    def toy_callback(self, msg: [Toy]):
         """
         Update list of toy positions
 
@@ -82,7 +84,7 @@ class Controller(Node):
         """
         pass
 
-    def navigate_to_toy(self) -> bool:
+    def navigate_to_toy(self) -> DoraJob:
         """
         Calculate route to toy using router.
         Convert route to robot movements, send service call to actuators.
@@ -90,19 +92,53 @@ class Controller(Node):
         Returns:
             job status
         """
-        pass
+        (route, self.toy) = self.router.next_retrieve_pt(self.map, self.toy_sub_, self.gps_sub_)
+        #TODO: Convert current position get from gps to the format np.array([x,y]) to stay the same page with router
+        success = self.navigate(route)
+        if success:
+            return DoraJob.NAV_TOY
+        else:
+            self.navigate_to_toy()
 
-    def navigate_to_storage(self) -> bool:
-        pass
+    def navigate_to_storage(self) -> DoraJob:
+        route = self.router.next_unload_pt(self.map, self.toy, self.gps_sub_)
+        #TODO: Convert current position get from gps to the format np.array([x,y]) to stay the same page with router
+        success = self.navigate(route)
+        if success:
+            return DoraJob.NAV_STORAGE
+        else:
+            return self.navigate_to_storage()
 
     def navigate(self, route: np.ndarray) -> bool:
-        pass
+        """
+        Publish format is a tuple contains MoveType and value (distance to move / angle to rotate)
+
+        Assume the gps will update the current position update automatically at a constant rate If not,
+        need to add timer in gps_node to call back often
+        """
+        for aim_point in route:
+            self.go_to_next_point(aim_point)
+            #TODO: Wait for some time for gps update?
+            if self.gps_sub_.x == aim_point.x & self.gps_sub_.y == aim_point.y:
+                # Make sure robot reach the destination every time
+                continue
+            else:
+                self.go_to_next_point(aim_point)
+        return True
+
+    def go_to_next_point(self, aim_point):
+        x_distance = aim_point.x - self.gps_sub_.x
+        y_distance = aim_point.y - self.gps_sub_.y
+        end_rotation = math.atan2(y_distance, x_distance)
+        rotate = end_rotation - self.gps_sub_.rot
+
+        # TODO: Add client part to build connection to send and get response from the robot. Will continue after get response
+
+        self.get_logger().info(f'Move command：turn for {rotate} angle')
+
+        distance = math.sqrt(x_distance ^ 2 + y_distance ^ 2)
+        # TODO: Add client part to build connection to send and get response from the robot. Will continue after get response
+
+        self.get_logger().info(f'Move command：move for {distance} meters')
 
 
-# Entry Point
-def main():
-    rclpy.init()
-    controller = Controller()
-    rclpy.spin(controller)
-    controller.destroy_node()
-    rclpy.shutdown()
