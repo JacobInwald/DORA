2c9feeb2d83791ecc3ee9d2a792ae5462b0c7b14
JacobInwald <Jacob.Inwald@Hotmail.com>
2024-02-27 11:46:15 +0000

    delete build

--- build/init/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/jacob/Desktop/DORA/install/init'
--- build/setup/colcon_command_prefix_setup_py.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/jacob/Desktop/DORA/install/dora_msgs/share/dora_msgs/package.sh"
