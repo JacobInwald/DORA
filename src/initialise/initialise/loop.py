b1275bb22c94e3fe4d99c85839a75e81cbc3d59e
JacobInwald <Jacob.Inwald@Hotmail.com>
2024-03-19 12:16:28 +0000

    fix: more bs ros shit

--- src/initialise/initialise/loop.py
+++ src/initialise/initialise/loop.py
@@ -11,17 +11,18 @@ class Loop(Node):
 
     def __init__(self):
         super().__init__('loop')
-        self.client_ = self.create_client(JobCmd, '/job')
+        self.cli_node_ = Node('loop_client')
+        self.job_cli_ = self.cli_node_.create_client(JobCmd, '/job')
+        self.loop_cli = self.cli_node_.create_service(
+            LoopCmd, '/loop', self.run)
         self.job = 5  # DEMO
         self.job_cmd = JobCmd.Request()
-        self.service_ = self.create_service(LoopCmd, '/loop', self.run)
-        # self.run()
 
     def run(self, request, response):
         while self.job != DoraJob.SCAN:
             self.job_cmd.job = self.job
-            future = self.client_.call_async(self.job_cmd)
-            rclpy.spin_until_future_complete(self.client_, future)
+            future = self.job_cli_.call_async(self.job_cmd)
+            rclpy.spin_until_future_complete(self.cli_node_, future)
             if future.result():
                 self.job = (self.job + 1) % len(DoraJob)
         response.status = False
